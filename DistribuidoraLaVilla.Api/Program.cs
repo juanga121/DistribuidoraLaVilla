b5df6d44081178e0756e799be7f93d162abbd3f4
Juan Pablo <giraldoatehortuapaj@gmail.com>
2026-08-06 20:14:50 -0500

    actual prod

--- DistribuidoraLaVilla.Api/Program.cs
+++ DistribuidoraLaVilla.Api/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", builder =>
     {
-        builder.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+        builder.WithOrigins("http://localhost:4200", "http://192.168.1.200").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
     });
 });
 
